b032be8e0773950f23c7928e3ddddc72bee94b00
Arthur Elleres <arthur.elleres@icen.ufpa.br>
2026-01-24 10:58:16 -0300

    feat(04): alterar processor para limpar os dados

--- src/03_taint_analysis/processor.py
+++ src/03_taint_analysis/processor.py
@@ -1,6 +1,16 @@
-# Processamento, porém sem sanitização dos dados
+import re
+
 def format_search_term(term):
+    """
+    PROCESSOR (Sanitizer):
+    Agora esta função aplica uma limpeza ativa (Sanitization).
+    """
     if not term:
         return "default"
     
-    return f"'{term.upper()}'"
+    # ESTRATÉGIA: Allowlist (Lista Branca)
+    # Remove tudo que NÃO for letra (a-z) ou número (0-9).
+    safe_term = re.sub(r'[^a-zA-Z0-9]', '', term)
+    
+    # Retorna o termo limpo e encapsulado
+    return f"'{safe_term.upper()}'"
